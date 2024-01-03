a7be575375d797d02bc7a2ae8aada00c4fb62a40
gilahorn <gilahorn21@gmail.com>
2024-01-03 08:47:50 -0800

    1

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,12 +1,18 @@
 package arrays;
 
+import java.awt.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
 	//2. create an array of 5 robots.
-
+Robot[] robots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for (int i = 0; i < robots.length; i++) {
+	robots[i] = new Robot();
+	robots[i].setY(600);
+	robots[i].setX(100+i*50);
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
@@ -19,4 +25,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
